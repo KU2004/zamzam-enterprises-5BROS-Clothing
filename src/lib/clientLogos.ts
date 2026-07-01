fe3eac6beac541d981ec3e069a21f12aeb70821c
KU2004 <111187493+KU2004@users.noreply.github.com>
2026-07-01 12:40:28 +0530

    Add files via upload

---
+++ src/lib/clientLogos.ts
@@ -0,0 +1,21 @@
+import carrierLogo from "../assets/carrier.jpeg";
+import gulfLogo from "../assets/gulf.png";
+import safeproLogo from "../assets/safepro.jpeg";
+import gscaltexLogo from "../assets/gscaltex.png";
+import projectFloorsLogo from "../assets/projectfloors.jpeg";
+import swarajLogo from "../assets/swaraj.png";
+import alRajeahLogo from "../assets/al-rajeh.png";
+import ensoOilsLogo from "../assets/ensooils.png";
+import gEnergyLogo from "../assets/g-energy.png";
+
+export const clientLogos = [
+  { name: "CARRIER", logo: carrierLogo },
+  { name: "GULF", logo: gulfLogo },
+  { name: "SAFEPRO", logo: safeproLogo },
+  { name: "GS CALTEX", logo: gscaltexLogo },
+  { name: "PROJECT FLOORS", logo: projectFloorsLogo },
+  { name: "SWARAJ", logo: swarajLogo },
+  { name: "AL-RAJEH", logo: alRajeahLogo },
+  { name: "ENSO OILS & LUBRICANTS", logo: ensoOilsLogo },
+  { name: "G-ENERGY", logo: gEnergyLogo },
+];
