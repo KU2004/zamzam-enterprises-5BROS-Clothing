e35886901af8cfcc0ce79f43c7b93ea8f479881d
KU2004 <111187493+KU2004@users.noreply.github.com>
2026-07-03 15:42:47 +0530

    Update contact information in contactInfo.ts

--- src/lib/contactInfo.ts
+++ src/lib/contactInfo.ts
@@ -1,7 +1,7 @@
 export const CONTACT = {
-  addressFull: "Chembur, Mumbai, Maharashtra 400089, India",
+  addressFull: "P.L. LOKHANDE MARG, CHHEDA NAGAR, CROSS ROAD,KADARIYA NAGAR, NEAR SHIVAM FOOD, Chembur, Mumbai, Maharashtra 400089",
   addressShort: "Mumbai, Maharashtra, India",
-  email: "5bros00027@gmail.com",
+  email: "contact@5brosclothing.com",
   phones: ["+91 7306 3333 12", "+91 9619 7879 89"],
 };
 
