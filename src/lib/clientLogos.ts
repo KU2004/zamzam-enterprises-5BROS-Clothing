c7fd4660604e25fd72307cc910bbd2944ef0c1ae
KU2004 <111187493+KU2004@users.noreply.github.com>
2026-07-03 15:22:19 +0530

    Update clientLogos.ts

--- src/lib/clientLogos.ts
+++ src/lib/clientLogos.ts
@@ -3,7 +3,6 @@ import gulfLogo from "../assets/gulf.png";
 import safeproLogo from "../assets/safepro.jpeg";
 import gscaltexLogo from "../assets/gscaltex.png";
 import projectFloorsLogo from "../assets/projectfloors.jpeg";
-import swarajLogo from "../assets/swaraj.png";
 import alRajeahLogo from "../assets/al-rajeh.png";
 import ensoOilsLogo from "../assets/ensooils.png";
 import gEnergyLogo from "../assets/g-energy.png";
@@ -14,7 +13,6 @@ export const clientLogos = [
   { name: "SAFEPRO", logo: safeproLogo },
   { name: "GS CALTEX", logo: gscaltexLogo },
   { name: "PROJECT FLOORS", logo: projectFloorsLogo },
-  { name: "SWARAJ", logo: swarajLogo },
   { name: "AL-RAJEH", logo: alRajeahLogo },
   { name: "ENSO OILS & LUBRICANTS", logo: ensoOilsLogo },
   { name: "G-ENERGY", logo: gEnergyLogo },
