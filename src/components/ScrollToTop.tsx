79902cafbc2b0bec2febfe563f515ce6976758c9
KU2004 <111187493+KU2004@users.noreply.github.com>
2026-07-10 18:31:03 +0530

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,15 +2,25 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname, search } = useLocation();
+  const { pathname, search, hash } = useLocation();
 
   useEffect(() => {
+    if (hash) {
+      const id = hash.replace("#", "");
+      const element = document.getElementById(id);
+
+      if (element) {
+        element.scrollIntoView({ block: "start", behavior: "smooth" });
+        return;
+      }
+    }
+
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "instant" as ScrollBehavior,
     });
-  }, [pathname, search]);
+  }, [pathname, search, hash]);
 
   return null;
 }
