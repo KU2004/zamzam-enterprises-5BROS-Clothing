4d397586d0abaa1ee1de0c69a70f26465122fdb7
KU2004 <111187493+KU2004@users.noreply.github.com>
2026-08-06 12:15:01 +0530

    Format addressFull for better readability

--- src/lib/contactInfo.ts
+++ src/lib/contactInfo.ts
@@ -1,5 +1,5 @@
 export const CONTACT = {
-  addressFull: "P.L. LOKHANDE MARG, QADRIYA NAGAR, CHEMBUR, MUMBAI-400089 (INDIA)",
+  addressFull: "P.L. LOKHANDE MARG,\nQADRIYA NAGAR, CHEMBUR\nMUMBAI-400089 (INDIA)",
   addressShort: "Mumbai, Maharashtra, India",
   email: "contact@5brosclothing.com",
   phones: ["+91 7306 333 312", "+91 9619 787 989"],
